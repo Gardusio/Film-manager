dba97c56d40f36c6177a4f2442985f21223560f5
Gardusio <fabiobarbieri.dev@gmail.com>
2023-11-03 17:55:04 +0100

    refactoring

--- src/auth/authValidator.js
+++ src/auth/authValidator.js
@@ -1,4 +1,5 @@
 import { validator } from "../validation/validator.js";
+
 import LoginRequestSchema from '../../api/schemas/dtos/auth/LoginRequest.json' assert { type: 'json' };
 
 const validateLoginRequest = validator.validate({ body: LoginRequestSchema })
